517e4784a28a0f071204d80859a712dd0362300e
bjb28 <bryce.beuerlein@cisa.dhs.gov>
2020-06-16 14:04:31 -0400

    Improved doc strings in test_tools.py

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -15,13 +15,13 @@ class TestComplete:
         "campaigns", [{"1": "RV0000-C1", "2": "RV0000-C2", "3": "RV0000-C3"}]
     )
     def test_get_campaign_id_found(self, campaigns):
-        """Test finding campaign id by campaign name when successful."""
+        """Verify correct campaign id is returned when a valid campaign name is provided."""
         assert get_campaign_id("RV0000-C2", campaigns) == "2"
 
     @pytest.mark.parametrize(
         "campaigns", [{"1": "RV0000-C1", "2": "RV0000-C2", "3": "RV0000-C3"}]
     )
     def test_get_campaign_id_not_found(self, campaigns):
-        """Test getting campaign id bu campaign name when unsuccessful."""
+        """Verify LookupError is raised when searching for unknown campaign id."""
         with pytest.raises(LookupError):
             get_campaign_id("RV0000-C6", campaigns)
