dce117cffccc486fcfddac74f8dd9ec6dd6cb770
Nick Viola <nick.viola@associates.cisa.dhs.gov>
2021-07-23 13:31:30 -0500

    Fixing typo in assessment builder test function name.

--- tests/test_assessment_builder.py
+++ tests/test_assessment_builder.py
@@ -10,7 +10,7 @@ from assessment.builder import set_time_zone
 class TestSetTimezone:
     """Test setting timezone logic."""
 
-    def test_radiolist_dialog_run_callsed(self):
+    def test_radiolist_dialog_run_called(self):
         """Verify radiolist_dialog run called once."""
         with patch("assessment.builder.radiolist_dialog") as run_patch:
             set_time_zone()
