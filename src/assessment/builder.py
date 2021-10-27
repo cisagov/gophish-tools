22c12bfad1e5e5a4f8169afa0f68ceb2f99f1df7
JCantu248 <jcantu248@gmail.com>
2021-10-27 14:34:14 -0500

    Bump project version from 0.0.10-rc.1 to 0.0.10-rc.2

--- src/assessment/builder.py
+++ src/assessment/builder.py
@@ -576,12 +576,12 @@ def build_pages(id_):
         logging.info(f"Building Page {page_num + 1}")
         temp_page = Page()
         auto_forward = yes_no_prompt("    Will this page auto forward?")
+        temp_page.capture_credentials = False
+        temp_page.capture_passwords = False
 
         if auto_forward == "yes":
 
             setattr(temp_page, "name", f"{id_}-{page_num+1}-AutoForward")
-            temp_page.capture_credentials = False
-            temp_page.capture_passwords = False
             temp_page.html = AUTO_FORWARD
             temp_page.redirect_url = get_input("    URL to Redirect to:")
 
@@ -590,12 +590,7 @@ def build_pages(id_):
 
             forward = yes_no_prompt("    Will this page forward after action? (yes/no)")
             if forward == "yes":
-                temp_page.capture_credentials = False
                 temp_page.redirect_url = get_input("    URL to Redirect to:")
-            else:
-                temp_page.capture_credentials = False
-
-            temp_page.capture_passwords = False
 
             # Receives the file name and checks if it exists.
             while True:
